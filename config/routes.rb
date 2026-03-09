923871a809ec13df0110a15caf53cd8e43a80a81
はるまま <yuki.6189@gmail.com>
2026-03-09 11:27:19 +0900

    ユーザー登録途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#top"
+  resources :users, only: %i[new create]
 end
